6c26a6053d8608595b79c62bc3399d76ce651b73
ahmad naser eddin <joker.soft.jo@gmail.com>
2026-09-25 20:12:28 +0300

    Build UCP Core protocol foundation

---
+++ core/lib/src/protocol/ucp_protocol.dart
@@ -0,0 +1,8 @@
+/// Current UCP protocol version.
+class UcpProtocol {
+  static const int major = 1;
+  static const int minor = 0;
+  static const String version = '$major.$minor';
+
+  const UcpProtocol._();
+}
